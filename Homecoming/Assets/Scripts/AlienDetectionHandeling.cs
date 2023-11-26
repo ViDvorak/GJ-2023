090524a7562d850ce714532c6900952da2e9d1c0
Vit Dvorak <vidvorak0@gmail.com>
2023-11-26 15:37:16 +0100

    level 1 2 renamed and detection is now dependent on  player items

--- Homecoming/Assets/Scripts/AlienDetectionHandeling.cs
+++ Homecoming/Assets/Scripts/AlienDetectionHandeling.cs
@@ -1,6 +1,34 @@
+using System;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEditor.Search;
 using UnityEngine;
 
 public class AlienDetectionHandeling : MonoBehaviour
 {
-    public float AweranessIncreasSpeed = 0.5f;
+    public Item[] ItemsLoweringAweraness;
+
+    [SerializeField]
+    private float aweranessIncreasSpeed = 0.5f;
+
+    public float minAweranesIncreasSpeed = 0f;
+
+    public float AweranessIncreasSpeed { get {
+        int restrectedItemOwnedCount = 0;
+
+        foreach (Item item in ItemsLoweringAweraness)
+        {
+            if (GlobalGameState.HasItems(item))
+            {
+                restrectedItemOwnedCount++;
+            }
+        }
+        float parameter;
+        if (ItemsLoweringAweraness.Length > 0)
+            parameter = 1 - (float)restrectedItemOwnedCount / ItemsLoweringAweraness.Length;
+        else
+            parameter = 1;
+        return minAweranesIncreasSpeed + (aweranessIncreasSpeed - minAweranesIncreasSpeed) * parameter;
+        }
+    }
 }
