7d58131de1b30354cc5ff759ffc1328a55643e91
Petr Kotáb <petrkotab99@gmail.com>
2023-11-26 15:53:37 +0100

    Tooltips for are changers.

--- Homecoming/Assets/Scripts/SceneChangerController.cs
+++ Homecoming/Assets/Scripts/SceneChangerController.cs
@@ -8,6 +8,10 @@ public class SceneChangerController : MonoBehaviour
 {
     private PlayerInputActions playerInputActions;
 
+    private GameObject tooltips;
+    private GameObject tooltipText;
+    private GameObject neededItemsText;
+
     /// <summary>
     /// Scene which will become active upon entering scene changer area. Scene needs to be included in build settings
     /// (file -> build settings).
@@ -18,10 +22,38 @@ public class SceneChangerController : MonoBehaviour
     /// </summary>
     public Item RequiredItems;
 
-    private void Awake()
+    private void Start()
     {
+        tooltips = transform.parent.Find("Tooltips").gameObject;
+        tooltipText = transform.parent.Find("Tooltips").Find("Tooltip Text").gameObject;
+        neededItemsText = transform.parent.Find("Tooltips").Find("Needed items Text").gameObject;
+
         playerInputActions = new PlayerInputActions();
         playerInputActions.Enable();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>() == null)
+            return;
+
+        tooltips.SetActive(true);
+
+        TextMesh textMesh = neededItemsText.GetComponent<TextMesh>();
+        textMesh.text = textMesh.text.Replace("#", RequiredItems.ToString());
+        Debug.Log(textMesh.text);
+
+        bool hasItems = GlobalGameState.HasItems(RequiredItems);
+        tooltipText.SetActive(hasItems);
+        neededItemsText.SetActive(!hasItems);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>() == null)
+            return;
+
+        tooltips.SetActive(false);
     }
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -31,7 +63,12 @@ public class SceneChangerController : MonoBehaviour
         if (playerController == null)
             return;
 
-        if (playerInputActions.Player.Interaction.IsPressed() && GlobalGameState.HasItems(RequiredItems))
+        bool hasItems = GlobalGameState.HasItems(RequiredItems);
+
+        tooltipText.SetActive(hasItems);
+        neededItemsText.SetActive(!hasItems);
+
+        if (playerInputActions.Player.Interaction.IsPressed() && hasItems)
         {
             SceneManager.LoadScene(Scene.name);
         }
