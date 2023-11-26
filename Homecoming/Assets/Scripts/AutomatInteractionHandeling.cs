a70ece93e22c7cf89ed217e3bc5416dabe2f1573
Vit Dvorak <vidvorak0@gmail.com>
2023-11-26 13:45:08 +0100

    Ticket machine working with conditions

--- Homecoming/Assets/Scripts/AutomatInteractionHandeling.cs
+++ Homecoming/Assets/Scripts/AutomatInteractionHandeling.cs
@@ -1,24 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class AutomatInteractionHandeling : MonoBehaviour
 {
     public bool isInteractable = false;
+    public bool isIntersectingWithTicketMachine = false;
     public GameObject text;
 
     PlayerInputActions input;
+    TextMesh textComponent;
 
     public void Awake()
     {
         input = new PlayerInputActions();
         input.Enable();
+        textComponent = text.GetComponent<TextMesh>();
+        Assert.IsNotNull(textComponent);
     }
 
     public void Update()
     {
-        if ( !GlobalGameState.HasItems(Item.Ticket) && input.Player.Interaction.WasPressedThisFrame())
+        if ( !GlobalGameState.HasItems(Item.Ticket) && input.Player.Interaction.WasPressedThisFrame() && isIntersectingWithTicketMachine)
         {
             GlobalGameState.PickUpItem(Item.Ticket);
         }
@@ -26,19 +33,27 @@ public class AutomatInteractionHandeling : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (isInteractable)
+        Debug.Log($"global State {GlobalGameState.PlayerItems}");
+
+        isIntersectingWithTicketMachine = true;
+        if (isInteractable && !GlobalGameState.HasItems(Item.Ticket))
         {
             text.SetActive(true);
+            if (GlobalGameState.HasItems(Item.Money))
+            {
+                textComponent.text = "Press E to buy a ticket";
+            }
+            else
+            {
+                textComponent.text = "You do not have any money to buy a ticket";
+            }
 
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (isInteractable)
-        {
-            text.SetActive(false);
-
-        }
+        text.SetActive(false);
+        isIntersectingWithTicketMachine = false;
     }
 }
--- Homecoming/Assets/Scripts/Item.cs
+++ Homecoming/Assets/Scripts/Item.cs
@@ -3,9 +3,10 @@ using System;
 [Flags]
 public enum Item
 {
-    None  = 0x00,
-    Scarf = 0x01,
-    Leaf  = 0x02,
-    Hat   = 0x04,
-    Ticket   = 0x08,
+    None    = 0x00,
+    Scarf   = 0x01,
+    Leaf    = 0x02,
+    Hat     = 0x04,
+    Ticket  = 0x08,
+    Money   = 0x20,
 }
